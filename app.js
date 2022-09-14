7f1bc910bd1c28fbf31a15db7aa8f9d104d58a1d
Wojtek <wojtekgg93@gmail.com>
2022-09-14 12:38:57 +0200

    added port variable

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res)=>{
+
+
+app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(3000, ()=>{
+    console.log(`listening on port ${port}`)
+})
 
