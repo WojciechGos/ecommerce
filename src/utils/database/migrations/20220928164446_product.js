d07bd37ab1f0ce160409ab62626d945bb7d7effe
Wojtek <wojtekgg93@gmail.com>
2022-10-10 20:05:36 +0200

    updated 'database' directory:
    -changed 'type' column from string to enum
    -updated 'image' column to 'image_name'

--- src/utils/database/migrations/20220928164446_product.js
+++ src/utils/database/migrations/20220928164446_product.js
@@ -2,25 +2,41 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
+
+const types = [
+    'Sofa Bed', 'Chair',
+    'Play Pool', 'Computer Desk',
+    'Standard Shoe Rack', 'Shoe Cabinet',
+    'Collapsible', 'Sofa',
+    'Sectional', 'Outdoor Chair',
+    'Interactive', 'Air Chair',
+    'Water Games', 'Entertainment Unit',
+    'Wine Rack', 'Spring',
+    'Coir', 'Teardrop',
+    'Desk Chair'
+]
+
+
+exports.up = function (knex) {
     return knex.schema.createTable('product', table => {
         table.increments('id')
         table.string('name').notNullable()
         table.integer('price').notNullable()
-        table.string('image').notNullable()
+        table.string('image_name').notNullable()
         table.string('description').notNullable()
         table.integer('rating').notNullable()
         table.string('brand').notNullable()
         table.json('specification').notNullable()
-        table.string('type').notNullable()
+        table.enu('type', types).notNullable()
         table.timestamps(true, true)
     })
 };
+
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema.dropTable('product')
 };
--- src/utils/database/seeds/product.js
+++ src/utils/database/seeds/product.js
@@ -3,14 +3,54 @@
  * @returns { Promise<void> } 
  */
 
+const products = require('./products.json')
+const uuid = require('uuid')
+const AWS = require('aws-sdk')
+
+const s3 = new AWS.S3({
+    credentials: {
+        accessKeyId: process.env.ACCES_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCES_KEY
+    }
+})
+
+exports.seed = async function (knex) {
+    // Deletes ALL existing entries
+    await knex('product').del()
 
 
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+    products.map(async (item) => {
+        const image_name = `${uuid.v1()}.jpeg`
+
+
+        const buffer = Buffer.from(item.image, 'base64')
+
+        const params = {
+            Bucket: process.env.AWS_S3_BUCKET_NAME,
+            Body: buffer,
+            Key: image_name
+        }
+        s3.putObject(params, (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            else
+                console.log(data);
+        })
+
+
+        const result = await knex('product').insert({
+            name: item.product_name,
+            price: item.retail_price,
+            image_name: image_name,
+            description: item.description,
+            rating: item.product_rating,
+            brand: item.brand,
+            specification: JSON.parse(item.product_specifications),
+            type: item.type
+        })
+        console.log(result)
+
+    })
 };
