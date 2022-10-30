5e6a7c4c0198d241c37391e2ff5ed02ef046f4e8
Wojtek <wojtekgg93@gmail.com>
2022-10-30 18:23:31 +0100

    added error handling directly from knex

--- src/utils/middleware/errorHandler.js
+++ src/utils/middleware/errorHandler.js
@@ -2,9 +2,13 @@ const {StatusCodes} = require('http-status-codes')
 
 const errorHandler = (err, req, res, next)=>{
     let message = err.message || 'Something went wrong'
-
     let status = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
-
+    
+    if (err.code == 23514){
+        message = 'Invalid data'
+        status = StatusCodes.BAD_REQUEST
+    }
+    // console.log(err);
     return res.status(status).send(message)
 }
 
