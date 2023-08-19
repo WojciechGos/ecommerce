29127455ffd646833c60a2c9acf6c9dc1a742a4e
Wojtek <wojtekgg93@gmail.com>
2023-08-19 14:29:47 +0200

    divided navbar components to 2 different files

--- frontend/src/components/Common/Navbar.jsx
+++ frontend/src/components/Common/Navbar.jsx
@@ -1,37 +1,11 @@
-
-const Tabs = ()=>{
-    return (
-        <nav className="navbar navbar-expand-md bg-navbar navbar-light sticky-top second-nav">
-            <div className="container">
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mb-2 mb-md-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/products">Shop All</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/aboutus">About Us</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/#">Contact</a>
-                        </li>
-                    </ul>
-                   
-                    <form className="d-flex my-2 my-lg-0">
-                        <input className="form-control border-bottom border-1" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn-search" type="submit"><i className="bi bi-search"></i></button>
-                    </form>
-
-                </div>
-            </div>
-        </nav>
-    )
-}
-
+import Tabs from "./Tabs"
+import { useContext } from "react"
+import CartContext from "../../context/CartContext"
 
 const Navbar = () => {
+    
+    const { setVisibility } = useContext(CartContext)
+
     return (
         <>
             <header>
@@ -50,7 +24,11 @@ const Navbar = () => {
                                 </a>
                             </li>
                             <li>
-                                <a className="nav-link" href="/#">
+                                <a className="nav-link" 
+                                    href="/#"
+                                    onMouseEnter={()=> setVisibility(true)}
+                                    onMouseLeave={()=> setVisibility(false)}    
+                                >
                                     <i className="bi bi-cart3"></i>
                                 </a>
                             </li>
